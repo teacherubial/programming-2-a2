8f3003e3a7d9e78e5c685adbcc7c96e05d6adc94
Tim Ubial <tubial@sd38.bc.ca>
2021-01-18 10:56:18 -0800

    Movement, bullets, etc.

--- pygame/shmup.py
+++ pygame/shmup.py
@@ -12,7 +12,7 @@ WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 SKY_BLUE = (95, 165, 228)
 WIDTH = 720
-HEIGHT = 1280
+HEIGHT = 1000
 TITLE = "Shmup"
 
 # Class Player, Enemies, Bullets
@@ -29,6 +29,10 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         """Move the player with the mouse"""
         self.rect.center = pygame.mouse.get_pos()
+
+        # Keep the player at the bottom of the screen
+        if self.rect.y < HEIGHT - 80:
+            self.rect.y = HEIGHT - 80
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -48,6 +52,26 @@ class Enemy(pygame.sprite.Sprite):
             self.x_vel *= -1
 
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, coords):
+        """
+        Arguments:
+            coords - tuple of x,y
+        """
+        super().__init__()
+
+        self.image = pygame.image.load("./images/bullet.png")
+        self.image = pygame.transform.scale(self.image, (21,36))
+
+        self.rect = self.image.get_rect()
+        self.rect.centerx, self.rect.y = coords
+
+        self.y_vel = -3
+
+    def update(self):
+        self.rect.y += self.y_vel
+
+
 def main():
     pygame.init()
 
@@ -61,12 +85,13 @@ def main():
     clock = pygame.time.Clock()
 
     # Sprite Groups
-    all_sprites = pygame.sprite.Group()    # to draw
-    enemy_sprites = pygame.sprite.Group()  # enemies
+    all_sprites = pygame.sprite.Group()     # to draw
+    enemy_sprites = pygame.sprite.Group()   # enemies
+    bullet_sprites = pygame.sprite.Group()  # bullets
 
     # Populate sprite Groups
     enemy = Enemy()
-    enemy.rect.y = 150                     # testing
+    enemy.rect.y = 150                      # testing
     all_sprites.add(enemy)
     enemy_sprites.add(enemy)
 
@@ -79,9 +104,25 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # create a bullet where the player is
+                if len(bullet_sprites) <= 3:
+                    bullet = Bullet(player.rect.midtop)
+                    all_sprites.add(bullet)
+                    bullet_sprites.add(bullet)
 
         # ----- LOGIC
         all_sprites.update()
+
+        # remove bullet if off screen
+        for bullet in bullet_sprites:
+            if bullet.rect.y < -20:
+                bullet.kill()
+
+            # collision
+            enemy_hit_group = pygame.sprite.spritecollide(bullet, enemy_sprites, True)
+            if len(enemy_hit_group) > 0:
+                bullet.kill()
 
         # ----- DRAW
         screen.fill(BLACK)
